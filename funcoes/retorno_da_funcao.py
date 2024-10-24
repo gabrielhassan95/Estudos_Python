f30096b6be790f25f693ab9eb57d1eb5f7dc9539
Gabriel <ghsammour01@gmail.com>
2024-10-24 11:00:36 -0300

    atz

--- funcoes/retorno_da_funcao.py
+++ funcoes/retorno_da_funcao.py
@@ -12,7 +12,7 @@ n1 = (int(input("Digite um número: ")))
 n2 = (int(input("Digite um número: ")))
 n3 = (int(input("Digite um número: ")))
 resultado = n1+n2+n3
-print(f"Soma de {n1,n2,n3} é = {resultado}")
+print(f"Soma de {n1,n2,n3} é {resultado}")
 
 numero = (int(input("Digite um número: ")))
 print("Antecessor e sucessor do número", (numero), "é",retorna_antecessor_e_sucessor(numero))
